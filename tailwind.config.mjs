fb558fa8571ea6900c2d4ed0df2affb02e148939
BeBraveBeKind <michael@rise-above.net>
2025-04-24 23:00:54 -0500

    ChatGPT site template

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+    content: ['./src/**/*.{astro,html,js}'],
+    theme: {
+      extend: {
+        colors: {
+          brandBlue: '#6D98BA',
+        },
+        fontFamily: {
+          sans: ['Montserrat', 'sans-serif'],
+        }
+      },
+    },
+    plugins: [],
+  };
