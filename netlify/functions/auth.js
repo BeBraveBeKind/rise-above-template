d8297fb4723b37b170df12e4e80879a2dfe81ea9
BeBraveBeKind <michael@rise-above.net>
2025-04-25 13:13:48 -0500

    AUTH.JS CHANGES

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,2 +1,38 @@
-const client_id = process.env.GITHUB_CLIENT_ID_TEMPLATE;
-const client_secret = process.env.GITHUB_CLIENT_SECRET_TEMPLATE;
+export async function handler(event, context) {
+    const params = event.queryStringParameters;
+    const code = params.code;
+  
+    if (!code) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "No code provided" }),
+      };
+    }
+  
+    const client_id = process.env.GITHUB_CLIENT_ID_TEMPLATE;
+    const client_secret = process.env.GITHUB_CLIENT_SECRET_TEMPLATE;
+  
+    const tokenRequest = await fetch(`https://github.com/login/oauth/access_token`, {
+      method: "POST",
+      headers: { Accept: "application/json" },
+      body: new URLSearchParams({
+        client_id,
+        client_secret,
+        code,
+      }),
+    });
+  
+    const tokenData = await tokenRequest.json();
+  
+    if (tokenData.error) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: tokenData.error }),
+      };
+    }
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tokenData),
+    };
+  }
