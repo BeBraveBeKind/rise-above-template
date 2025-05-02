7b1608f018dfd6438822ac87c7569a27108a3b2e
BeBraveBeKind <michael@rise-above.net>
2025-05-01 21:51:48 -0500

    registry confirmation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-const staticAdapter = () => ({
-  name: 'mock-adapter',
-  adapt() {
-    console.warn('Static adapter not found. Using mock adapter for local dev.');
-  }
+import staticAdapter from '@astrojs/adapter-static';
+
+export default defineConfig({
+  adapter: staticAdapter(),
 });
-export default defineConfig({ adapter: staticAdapter() });
